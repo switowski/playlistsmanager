62ac09bb0f72dce96954be02968a47a829df745e
switowski <witowski.sebastian@gmail.com>
2014-05-10 19:34:09 +0200

    More stuff implemented:
    * Modified the css, created a simple interface with buttons
    
    * JS how recognizes on which page we are
    
    * Added "permissions" for tabs in manifest

--- popup.js
+++ popup.js
@@ -8,6 +8,8 @@
  *
  * @type {string}
  */
+'use strict';
+
 var QUERY = 'kittens';
 
 var kittenGenerator = {
@@ -37,7 +39,7 @@ var kittenGenerator = {
    */
   requestKittens: function() {
     var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
+    req.open('GET', this.searchOnFlickr_, true);
     req.onload = this.showPhotos_.bind(this);
     req.send(null);
   },
@@ -69,15 +71,55 @@ var kittenGenerator = {
    * @private
    */
   constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
+    return 'http://farm' + photo.getAttribute('farm') +
+        '.static.flickr.com/' + photo.getAttribute('server') +
+        '/' + photo.getAttribute('id') +
+        '_' + photo.getAttribute('secret') +
+        '_s.jpg';
+  }
+};
+
+
+var playlistManager = {
+  // Create interface
+  createInterface: function(){
+    // The box is made by popup.html, we just need to create buttons here
+    var btn = document.createElement('button');
+    btn.textContent = 'Export playlist';
+    btn.onclick = this.exportPlaylist;
+    // var btn2 = $('<button>Import playlist</button>');
+    document.body.appendChild(btn);
+  },
+
+  exportPlaylist: function () {
+  // Function that is called when 'Export playlist' button is clicked
+  chrome.tabs.query({active: true, currentWindow: true}, function(tab) {
+    // this functions is async so call other functions based on the url here, not outside of this function
+    var activeUrl = tab[0].url;
+    if ((activeUrl.indexOf('grooveshark') > -1) && (activeUrl.indexOf('/playlist/') > -1))) {
+      // We are on Grooveshark website with a playlist
+      console.log(activeUrl);
+      console.log('Grooveshark website !');
+    } else if (activeUrl.indexOf('deezer') > -1) {
+      // We are on Deezer website
+    } else if (activeUrl.indexOf('deezer') > -1) {
+      // We are on Spotify website
+    } else {
+      // We are on different website, we can't perform anything here
+      alert('Sorry but this page is unsupported yet. Our programmers are working day and night so you will be able to export music from ' + activeUrl + ' in the nearest future.');
+      // TODO replace this stupid alert with HTML added to extension bubble that is displayed for 10 seconds and disappears later
+    }
+  });
+
+  },
+
+  parseGrooveshark: function () {
+  // parse the HTML, grab all metadata about songs and create XML files
+  $('.module-row').filter('.song');
   }
 };
 
 // Run the script when the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  kittenGenerator.requestKittens();
+  playlistManager.createInterface();
 });
